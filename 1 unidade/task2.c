57dfda3d060a32f1c022b73065e8e45018206d6d
Leandro Manuel Francisco <leandro_00010@outlook.com>
2025-08-22 12:10:18 -0300

    tasf for input and output

---
+++ 1 unidade/task2.c
@@ -0,0 +1,9 @@
+// c program to take user input
+#include <stdio.h>
+int main() {
+    int testInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger);
+    printf("Number: %d",testInteger);
+    return 0;
+}
